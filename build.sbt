19a4fb3c38c921c56e117ee91eecefcf6f402f8b
timurkuhtin <mr.timur.pipal@gmail.com>
2021-12-21 11:08:11 +0200

    com1

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "scala-actors"
+
+version := "0.1"
+
+scalaVersion := "2.10.3"
+
+libraryDependencies ++= Seq("org.scala-lang" % "scala-actors" % "2.10.3",
+  "com.typesafe.akka" % "akka-actor_2.10" % "2.2-M1")
+
---
+++ src/main/scala/HelloActor.scala
@@ -0,0 +1,19 @@
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+class HelloActor(myName: String) extends Actor {
+  def receive = {
+    // (2) changed these println statements
+    case "hello" => println("hello from %s".format(myName))
+    case _       => println("'huh?', said %s".format(myName))
+  }
+}
+
+object Main extends App {
+  val system = ActorSystem("HelloSystem")
+  // (3) changed this line of code
+  val helloActor = system.actorOf(Props(new HelloActor("Fred")), name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "buenos dias"
+}
